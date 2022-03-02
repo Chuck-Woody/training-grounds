d0a9ae977deb41c5fb7dedab76f8cdebb5516d92
chuck <you@example.com>
2022-03-02 12:32:30 -0800

    Battleship Project Passed Rspec

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,19 @@
 class Player
 
+    def get_move
+
+        begin
+        p 'enter a position with coordinates separated with a space like `4 7`'
+        
+        input = gets.chomp
+        
+            raise "please re-input your move" if input.split.length != 2
+        rescue
+            retry
+        end
+
+        move = input.split(" ")
+        move.map! {|ele| ele.to_i}
+    end
+
 end
